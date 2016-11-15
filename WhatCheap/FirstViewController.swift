d8959bd769754b8e27964b0de816a86f2d7c7b23
kamontat <kamontat_c@hotmail.com>
2016-11-15 15:11:10 +0700

    Initial Commit

---
+++ WhatCheap/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WhatCheap
+//
+//  Created by kamontat chantrachirathumrong on 11/15/2559 BE.
+//  Copyright © 2559 kamontat chantrachirathumrong. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
